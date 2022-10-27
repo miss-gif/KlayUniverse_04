0f0b625de248f8c98288b80cb94fc23d3735758b
miss <ddc.akari@gmail.com>
2022-10-28 08:23:11 +0900

    TEAM

--- js/slide.js
+++ js/slide.js
@@ -19,3 +19,26 @@ const swiper1 = new Swiper(".UNIVERSE .swiper", {
     },
   },
 });
+
+/* section TEAM 슬라이드 컨트롤 */
+
+const swiper2 = new Swiper(".TEAM .swiper", {
+  direction: "horizontal", // 수평 슬라이드
+  // autoplay: {
+  //   // 자동 재생 여부
+  //   // delay: 5000, // 5초마다 슬라이드 바뀜
+  // },
+  loop: true, // 반복 재생 여부
+  spaceBetween: 20, // 슬라이드 사이 여백
+  slidesPerView: 2, // 한 번에 보여줄 슬라이드 개수
+  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  // mousewheel: true, // 마우스 휠 기능
+  breakpoints: {
+    1275: {
+      slidesPerView: 6.7,
+    },
+    940: {
+      slidesPerView: 3.6,
+    },
+  },
+});
