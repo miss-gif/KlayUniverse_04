39f1bc044e487535b603936fd041082fa5bb5964
miss <ddc.akari@gmail.com>
2022-10-28 10:04:46 +0900

    GALLERY

--- js/slide.js
+++ js/slide.js
@@ -68,4 +68,9 @@ const swiper3 = new Swiper(".GALLERY .swiper", {
     },
   },
   slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
+  pagination: {
+    // 페이지 번호 사용 여부
+    el: ".swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 페이지 버튼 활성화
+  },
 });
