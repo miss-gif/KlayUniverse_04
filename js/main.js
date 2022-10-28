9a6a0bcfac98f2b8c3ae78b15bdf5888004fd387
miss <ddc.akari@gmail.com>
2022-10-28 16:09:36 +0900

    language

--- js/main.js
+++ js/main.js
@@ -113,3 +113,64 @@ const modalClose = document.querySelector(".modalClose");
 modalClose.addEventListener("click", function () {
   modalNotPage.classList.add("hide");
 });
+
+/* 언어 변경 버튼*/
+const languageBtn = document.querySelectorAll(".languageBtn>li>button");
+
+languageBtn[0].addEventListener("click", function () {
+  languageBtn[0].classList.add("active");
+  languageBtn[1].classList.remove("active");
+  document.querySelector(".s03").innerText = lang.en.s03.desc;
+  document.querySelector(".q2_01").innerText = lang.en.s04.q2_01;
+  document.querySelector(".q2_02").innerText = lang.en.s04.q2_02;
+  document.querySelector(".q2_03").innerText = lang.en.s04.q2_03;
+  document.querySelector(".q2_04").innerText = lang.en.s04.q2_04;
+  document.querySelector(".q2_05").innerText = lang.en.s04.q2_05;
+  document.querySelector(".q2_06").innerText = lang.en.s04.q2_06;
+  document.querySelector(".q2_07").innerText = lang.en.s04.q2_07;
+  document.querySelector(".q2_08").innerText = lang.en.s04.q2_08;
+  document.querySelector(".q3_01").innerText = lang.en.s04.q3_01;
+  document.querySelector(".q3_02").innerText = lang.en.s04.q3_02;
+  document.querySelector(".q3_03").innerText = lang.en.s04.q3_03;
+  document.querySelector(".q3_04").innerText = lang.en.s04.q3_04;
+  document.querySelector(".q3_05").innerText = lang.en.s04.q3_05;
+  document.querySelector(".q3_06").innerText = lang.en.s04.q3_06;
+  document.querySelector(".q3_07").innerText = lang.en.s04.q3_07;
+  document.querySelector(".q3_08").innerText = lang.en.s04.q3_08;
+  document.querySelector(".q3_09").innerText = lang.en.s04.q3_09;
+  document.querySelector(".q3_10").innerText = lang.en.s04.q3_10;
+  document.querySelector(".q3_11").innerText = lang.en.s04.q3_11;
+  document.querySelector(".q3_12").innerText = lang.en.s04.q3_12;
+  document.querySelector(".q3_13").innerText = lang.en.s04.q3_13;
+  document.querySelector(".s05").innerText = lang.en.s05.desc;
+  document.querySelector(".s06").innerText = lang.en.s06.desc;
+});
+
+languageBtn[1].addEventListener("click", function () {
+  languageBtn[0].classList.remove("active");
+  languageBtn[1].classList.add("active");
+  document.querySelector(".s03").innerText = lang.kr.s03.desc;
+  document.querySelector(".q2_01").innerText = lang.kr.s04.q2_01;
+  document.querySelector(".q2_02").innerText = lang.kr.s04.q2_02;
+  document.querySelector(".q2_03").innerText = lang.kr.s04.q2_03;
+  document.querySelector(".q2_04").innerText = lang.kr.s04.q2_04;
+  document.querySelector(".q2_05").innerText = lang.kr.s04.q2_05;
+  document.querySelector(".q2_06").innerText = lang.kr.s04.q2_06;
+  document.querySelector(".q2_07").innerText = lang.kr.s04.q2_07;
+  document.querySelector(".q2_08").innerText = lang.kr.s04.q2_08;
+  document.querySelector(".q3_01").innerText = lang.kr.s04.q3_01;
+  document.querySelector(".q3_02").innerText = lang.kr.s04.q3_02;
+  document.querySelector(".q3_03").innerText = lang.kr.s04.q3_03;
+  document.querySelector(".q3_04").innerText = lang.kr.s04.q3_04;
+  document.querySelector(".q3_05").innerText = lang.kr.s04.q3_05;
+  document.querySelector(".q3_06").innerText = lang.kr.s04.q3_06;
+  document.querySelector(".q3_07").innerText = lang.kr.s04.q3_07;
+  document.querySelector(".q3_08").innerText = lang.kr.s04.q3_08;
+  document.querySelector(".q3_09").innerText = lang.kr.s04.q3_09;
+  document.querySelector(".q3_10").innerText = lang.kr.s04.q3_10;
+  document.querySelector(".q3_11").innerText = lang.kr.s04.q3_11;
+  document.querySelector(".q3_12").innerText = lang.kr.s04.q3_12;
+  document.querySelector(".q3_13").innerText = lang.kr.s04.q3_13;
+  document.querySelector(".s05").innerText = lang.kr.s05.desc;
+  document.querySelector(".s06").innerText = lang.kr.s06.desc;
+});
