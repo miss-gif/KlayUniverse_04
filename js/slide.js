cd814da5c42bacc8016ea1b24bea369bb03bea5a
miss <ddc.akari@gmail.com>
2022-10-28 09:43:32 +0900

    save

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,4 @@
 /* section 섹션3 UNIVERSE 슬라이드 컨트롤 */
-
 const swiper1 = new Swiper(".UNIVERSE .swiper", {
   direction: "horizontal", // 수평 슬라이드
   autoplay: {
@@ -8,8 +7,8 @@ const swiper1 = new Swiper(".UNIVERSE .swiper", {
   },
   loop: true, // 반복 재생 여부
   spaceBetween: 20, // 슬라이드 사이 여백
-  slidesPerView: 2, // 한 번에 보여줄 슬라이드 개수
-  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수
+  // 반응형 $size이상에서 적용
   breakpoints: {
     1275: {
       slidesPerView: 4.7,
@@ -18,33 +17,38 @@ const swiper1 = new Swiper(".UNIVERSE .swiper", {
       slidesPerView: 3.7,
     },
   },
+  slidesPerView: 2, // 한 번에 보여줄 슬라이드 개수
 });
 
 /* section 섹션5 TEAM 슬라이드 컨트롤 */
-
 const swiper2 = new Swiper(".TEAM .swiper", {
   direction: "horizontal", // 수평 슬라이드
-  // autoplay: {
-  //   // 자동 재생 여부
-  //   // delay: 5000, // 5초마다 슬라이드 바뀜
-  // },
+  autoplay: {
+    // 자동 재생 여부
+    delay: 3500, // 3.5초마다 슬라이드 교체
+  },
   loop: true, // 반복 재생 여부
   spaceBetween: 20, // 슬라이드 사이 여백
-  slidesPerView: 2, // 한 번에 보여줄 슬라이드 개수
-  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
-  // mousewheel: true, // 마우스 휠 기능
+  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수
+  // 반응형 $size이상에서 적용
   breakpoints: {
-    1275: {
+    1400: {
       slidesPerView: 6.7,
     },
-    940: {
-      slidesPerView: 3.6,
+    1275: {
+      slidesPerView: 5.7,
+    },
+    740: {
+      slidesPerView: 3.7,
+    },
+    320: {
+      slidesPerView: 2,
     },
   },
+  slidesPerView: 2, // 한 번에 보여줄 슬라이드 개수
 });
 
 /* section 섹션7 GALLERY 슬라이드 컨트롤 */
-
 const swiper3 = new Swiper(".GALLERY .swiper", {
   direction: "horizontal", // 수평 슬라이드
   autoplay: {
@@ -53,14 +57,15 @@ const swiper3 = new Swiper(".GALLERY .swiper", {
   },
   loop: true, // 반복 재생 여부
   spaceBetween: 20, // 슬라이드 사이 여백
-  slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
-  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수
+  // 반응형 $size이상에서 적용
   breakpoints: {
     1275: {
       slidesPerView: 3.7,
     },
-    940: {
+    720: {
       slidesPerView: 2.7,
     },
   },
+  slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
 });
